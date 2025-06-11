e203d0a48c32e3c0c7571c20e5a0bab88ccec29a
jangseungwon08 <jangseungwon9819@gmail.com>
2025-06-11 22:17:42 +0900

    초기값 세팅 완료

---
+++ src/main/java/com/welab/k8s_backend_alim/api/open/BackendAlimController.java
@@ -0,0 +1,20 @@
+package com.welab.k8s_backend_alim.api.open;
+
+import com.welab.k8s_backend_alim.common.dto.ApiResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+@RequestMapping(value = "/api/alim/v1", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+public class BackendAlimController {
+    @GetMapping(value = "/hello")
+    public ApiResponseDto<String> hello(){
+        return ApiResponseDto.createOk("알림 백엔드 호출 됨");
+    }
+}
